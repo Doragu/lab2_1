de475b8657178490cf1231ad6a444ae173150ced
Krzysztof Chybicki <krysiek97@interia.eu>
2020-03-31 12:23:47 +0200

    testy_v1 testowanie elementu znajdującego się w jednoelementowej sekwencji

--- src/main/java/edu/iis/mto/bsearch/SearchResult.java
+++ src/main/java/edu/iis/mto/bsearch/SearchResult.java
@@ -24,13 +24,11 @@ public class SearchResult {
         return position;
     }
 
-    @Override
-    public int hashCode() {
+    @Override public int hashCode() {
         return Objects.hash(position);
     }
 
-    @Override
-    public boolean equals(Object obj) {
+    @Override public boolean equals(Object obj) {
         if (this == obj) {
             return true;
         }
@@ -44,8 +42,7 @@ public class SearchResult {
         return position == other.position;
     }
 
-    @Override
-    public String toString() {
+    @Override public String toString() {
 
         return isFound() ? "( element at position " + position + ")" : "element not found";
     }
---
+++ src/test/java/BinarySearchTests.java
@@ -0,0 +1,21 @@
+import edu.iis.mto.bsearch.BinarySearch;
+import edu.iis.mto.bsearch.SearchResult;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BinarySearchTests {
+
+    private static final int[] SEARCH_SEQUENCE = {2};
+    private static final BinarySearch BINARY_SEARCH = BinarySearch.create();
+
+    @Test void searchForElementInSequence() {
+        final int KEY = 2;
+        final int EXPECTED_POSITION = 0;
+
+        SearchResult searchResult = BINARY_SEARCH.search(KEY, SEARCH_SEQUENCE);
+
+        Assertions.assertTrue(searchResult.isFound());
+        Assertions.assertEquals(searchResult.getPosition(), EXPECTED_POSITION);
+        Assertions.assertEquals(SEARCH_SEQUENCE[searchResult.getPosition()], KEY);
+    }
+}
